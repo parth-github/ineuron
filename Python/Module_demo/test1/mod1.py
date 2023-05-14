5fcafaa90f5d55e1450ddb14bb018dfcfcdeedd5
Smriti <linuxriti@gmail.com>
2023-05-14 23:13:02 +0530

    Assignment3 solution uploaded

---
+++ Python/Module_demo/test1/mod1.py
@@ -0,0 +1,8 @@
+def func1():
+    return "mod1 func1"
+
+def func2():
+    return "mod1 func2"
+
+def func3():
+    return "mod1 func3"
---
+++ Python/Module_demo/test1/mod2.py
@@ -0,0 +1,5 @@
+def funct21():
+    print("mod2 funct1")
+
+def funct22():
+    print("mod2 funct2")
